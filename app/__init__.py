6ae423f17d6c6e30ab1c34f3b4fe9fbeb3b5fc63
joshua <joshua.mbilli@student.moringaschool.com>
2026-07-29 12:49:10 +0300

    imported root config,added bcrypt and jwt

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,21 @@
 from flask import Flask
-from app.extensions import db, migrate
+from app.extensions import db, migrate, bcrypt, jwt
+from config import Config
 
 def create_app():
     app = Flask(__name__)
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///bakery.db"
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config.from_object(Config)
 
     db.init_app(app)
     migrate.init_app(app, db)
+    bcrypt.init_app(app)
+    jwt.init_app(app)
 
     # Import models so Flask-Migrate can detect them
     from app.models.models import Product, Order, OrderItem, Admin
+    
+    from app.routes.admin_routes import admin_bp
+    app.register_blueprint(admin_bp)
 
     return app
